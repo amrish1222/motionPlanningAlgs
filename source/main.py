681572981cde44cdd3f4d7269148da45926ae1f9
amrish1222 <amrish1222@gmail.com>
2020-03-05 16:42:15 -0500

    added main function for execution

---
+++ source/main.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from environment import Environment
+
+if __name__ == "__main__":
+    resolution = 0.05
+    botRadius = 0
+    
+    Env = Environment(resolution, botRadius)
+    
