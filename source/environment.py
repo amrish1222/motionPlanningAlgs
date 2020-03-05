333146940c3a93ad73ca68255bc1d55002beef2e
amrish1222 <amrish1222@gmail.com>
2020-03-05 17:43:34 -0500

    updated reset and render function for obsMap and running map

--- source/environment.py
+++ source/environment.py
@@ -10,6 +10,8 @@ class Environment:
     def __init__(self, resolution, botRadius):
         self.res = resolution
         self.totalClearance = botRadius
+        self.startPt = [0.0, 0.0]
+        self.goalPt = [11.0,10.0]
         self.MAP_X, self.MAP_Y = 11.1, 10.1
         self.GRID_NX, self.GRID_NY = int(self.MAP_X/self.res), int(self.MAP_Y/self.res)
         self.obstacleSet = self.getObstacleSet()
@@ -28,8 +30,11 @@ class Environment:
         return outputSet
     
     def isPtInObs(self, x, y):
-        temp = (math.floor(x/self.res), math.floor(y/self.res))
+        temp = self.getGridPt([x,y])
         return temp in self.obstacleSet
+    
+    def getGridPt(self,pt):
+        return (math.floor(pt[0]/self.res), math.floor(pt[1]/self.res))
     
     def updateMap(self,display,pt,val):
         # For Display
@@ -72,7 +77,9 @@ class Environment:
         return obsMap
     
     def render(self):
-        toDisplay = cv2.resize(self.runningMap,(1000, self.GRID_NY * 1000 // self.GRID_NX), interpolation = cv2.INTER_AREA)
+        self.runningMap = self.updateMap(self.runningMap,self.getGridPt(self.startPt),1)
+        self.runningMap = self.updateMap(self.runningMap,self.getGridPt(self.goalPt),2)
+        toDisplay = cv2.resize(self.runningMap,(self.GRID_NY * 1000 // self.GRID_NX,1000), interpolation = cv2.INTER_AREA)
         cv2.imshow("",np.rot90(toDisplay,1))
         cv2.waitKey(1)
     
