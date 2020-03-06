b543acfd686e13c44c2ced17d4017e6c5bfb9c84
amrish1222 <amrish1222@gmail.com>
2020-03-06 13:48:38 -0500

    Executed and tested dijkstra algorithm

--- source/main.py
+++ source/main.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 from environment import Environment
+from dijkstra import Dijkstra
 
 if __name__ == "__main__":
-    resolution = 0.05
+    resolution = 0.1
     botRadius = 0
-    Env = Environment(resolution, botRadius)
-    Env.render()
+    env = Environment(resolution, botRadius)
+    env.render()
+    
+    alg = Dijkstra(env)
+    alg.executeAlg()
